bdc48cd440c45c739ba346153609a8d1d91b0019
Oscar Leal <oscarleal@Oscars-MacBook-Pro.local>
2024-11-26 12:36:13 -0600

    working on UI for login page

--- app/login.tsx
+++ app/login.tsx
@@ -4,78 +4,86 @@ import {
   TextInput,
   Text,
   StyleSheet,
-  ViewStyle,
-  TextStyle,
-  TextInputProps,
-  Button,
 } from 'react-native';
+import { useTheme, Layout, Button } from '@ui-kitten/components';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import { useTheme,Layout } from '@ui-kitten/components';
 const login = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-
+  
   const handleSubmit = () => {
     setName('');
     setEmail('');
   };
 
-  const theme=useTheme()
-
+  const theme = useTheme();
 
   return (
-    <Layout style={{ flex: 1, backgroundColor: theme['color-basic-800'] }}>
-
-      <View style={styles.form}>
-        <Text style={styles.heading}>
-          Login
-        </Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Name"
-          onChangeText={(text) => setName(text)}
-          value={name}
-        />
-        <TextInput
-          style={[styles.input,
-            {
-              borderColor: theme['color-primary-800'], 
-            },]}
-          placeholder="Email"
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          keyboardType="email-address"
-        />
-        <Button title="Submit" onPress={handleSubmit} />
-      </View>
-    </Layout>
+    <LinearGradient colors={['black', 'white']} style={styles.background}>
+      <Layout style={styles.container}>
+        <View style={[styles.form, { backgroundColor: theme['color-basic-100'] }]}>
+          <Text style={styles.heading}>Login</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            onChangeText={(text) => setName(text)}
+            value={name}
+          />
+          <TextInput
+            style={[
+              styles.input,
+              { borderColor: theme['color-primary-800'] },
+            ]}
+            placeholder="Email"
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            keyboardType="email-address"
+          />
+          <Button onPress={handleSubmit}>Log in</Button>
+        </View>
+      </Layout>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
-  heading: {
-   display: 'flex',
-   justifyContent: 'center',
-   color: 'white',
-   marginBottom: 4
+  background: {
+    flex: 1,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+  heading: {
+    fontFamily: 'sans',
+    fontSize: 20,
+    fontWeight: 'semibold',
+    color: 'black',
+    marginBottom: 10,
   },
   form: {
-    flex: 1,
-    backgroundColor: 'primary-color-100',
+    display: 'flex',
     justifyContent: 'center',
-    height: 'auto',
+    alignItems: 'center',
+    width: 300, 
+    height: 300, 
     padding: 20,
+    borderRadius: 8,
   },
   input: {
     borderWidth: 1,
-    borderColor: 'color-primary-800',
+    borderColor: 'gray',
     padding: 10,
     marginBottom: 10,
+    width: '100%', 
+    borderRadius: 4,
   },
 });
 
