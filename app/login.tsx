3bb7a4ea9c5c507e3b4b31237e9efec2bec6b32b
Oscar Leal <oscarleal234@gmail.com>
2024-11-26 18:25:09 -0600

    adding securoity

--- app/login.tsx
+++ app/login.tsx
@@ -1,49 +1,51 @@
 import React, { useState } from 'react';
-import {
-  View,
-  TextInput,
-  Text,
-  StyleSheet,
-} from 'react-native';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
 import { useTheme, Layout, Button } from '@ui-kitten/components';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const login = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  
+
   const handleSubmit = () => {
-    setName('');
+    setPassword('');
     setEmail('');
   };
 
   const theme = useTheme();
 
   return (
-   
-      <Layout style={[styles.container, {backgroundColor: theme['color-base-700']}]}>
-        <View style={[styles.form, { backgroundColor: theme['color-basic-100'] }]}>
-          <Text style={styles.heading}>Login</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Name"
-            onChangeText={(text) => setName(text)}
-            value={name}
-          />
-          <TextInput
-            style={[
-              styles.input,
-              { borderColor: theme['color-primary-100'] },
-            ]}
-            placeholder="Email"
-            onChangeText={(text) => setEmail(text)}
-            value={email}
-            keyboardType="email-address"
-          />
-          <Button onPress={handleSubmit}>Log in</Button>
-        </View>
-      </Layout>
+    <Layout
+      style={[styles.container, { backgroundColor: theme['color-base-700'] }]}
+    >
+      <View
+        style={[styles.form, { backgroundColor: theme['color-basic-100'] }]}
+      >
+        <Text style={styles.heading}>Login</Text>
+        <Text style={styles.text}>Email:</Text>
 
+        <TextInput
+          style={[
+            styles.input,
+            {
+              borderColor: theme['color-primary-800'],
+            },
+          ]}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={[styles.input, { borderColor: theme['color-primary-800'] }]}
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry={true}
+        />
+        <Button onPress={handleSubmit}>Log in</Button>
+      </View>
+    </Layout>
   );
 };
 
@@ -58,8 +60,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   heading: {
     fontFamily: 'sans',
@@ -68,12 +70,18 @@ const styles = StyleSheet.create({
     color: 'black',
     marginBottom: 10,
   },
+  text: {
+    fontSize: 15,
+
+    marginBottom: 2,
+    width: '100%',
+  },
   form: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    width: 300, 
-    height: 300, 
+    width: 300,
+    height: 300,
     padding: 20,
     borderRadius: 8,
   },
@@ -82,7 +90,7 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     padding: 10,
     marginBottom: 10,
-    width: '100%', 
+    width: '100%',
     borderRadius: 4,
   },
 });
